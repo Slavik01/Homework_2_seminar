95add19e519181e713b4e47e58c3f06ee89afa90
Slavik <slavik-skripka@yandex.ru>
2022-07-02 16:50:41 +0300

    new info

--- To/Program.cs
+++ To/Program.cs
@@ -4,8 +4,32 @@
 // 782 -> 72
 // 918 -> 98
 
-int number = new Random().Next(100, 1000);
-Console.Write(number);
-int digitOne = number / 100;
-int digitThree = number % 10;
-Console.WriteLine("Результат" + digitOne + digitThree);
+// int number = new Random().Next(100, 1000);
+// Console.Write(number);
+// int digitOne = number / 100;
+// int digitThree = number % 10;
+// Console.WriteLine("Результат" + digitOne + digitThree);
+
+// void name()
+// {
+//     Console.Write("rgfr3eg3");
+// }
+
+// name();
+
+
+// void name(string mas)
+// {
+//     Console.Write(mas);
+// }
+
+// name("hello");
+
+
+// void Sum(int x , int y)
+// {
+//     int result = x + y;
+//     Console.Write($"{x} + {y} = {result}");
+// }
+// Sum(10 , 15);
+
